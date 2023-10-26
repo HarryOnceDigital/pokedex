1d6a15fb7c0099626284ea42359d0a62d4a4c9f7
HarryOnceDigital <148277568+HarryOnceDigital@users.noreply.github.com>
2023-10-26 11:49:14 -0600

    Creando modelo
    Se crea un modelo que obtendrá los datos que mostrará la lista del pokedex

---
+++ lib/models/pokemon_model.dart
@@ -0,0 +1,21 @@
+class Pokemon {
+  final String name; //nombre del Pokémon
+  final String url; //url del Pokémon
+  late String imageUrl; //imagen del Pokémon
+
+  Pokemon({
+    required this.name,
+    required this.url,
+  }) {
+    final int id = int.parse(url.split('/').elementAt(6));
+    imageUrl =
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png';
+  }
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      name: json['name'],
+      url: json['url'],
+    );
+  }
+}
